1ba85c9b8105982e3ce6d74c0374dba18f9ab0e3
JaronArmiger <jaron.armiger@gmail.com>
2020-07-04 17:31:52 -0500

    create user-account and address-account associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_04_222244) do
+ActiveRecord::Schema.define(version: 2020_07_04_222832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_07_04_222244) do
     t.integer "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
   create_table "addresses", force: :cascade do |t|
@@ -32,6 +34,8 @@ ActiveRecord::Schema.define(version: 2020_07_04_222244) do
     t.integer "zipcode"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "account_id", null: false
+    t.index ["account_id"], name: "index_addresses_on_account_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -61,4 +65,6 @@ ActiveRecord::Schema.define(version: 2020_07_04_222244) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
+  add_foreign_key "addresses", "accounts"
 end
