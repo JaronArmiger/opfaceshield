e5fe41fc17ad8c5e0729be51a2d06d382cb1b654
JaronArmiger <jaron.armiger@gmail.com>
2020-07-06 16:53:55 -0500

    add edit links to account show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("webpack-icons-installer/bootstrap")
 
 import './bootstrap_custom.js'
 import '../stylesheets/application'
 import '../scripts/accounts_form.js'
+
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   root 'static_pages#home'
   resources :accounts
+  resources :addresses, except: [:index, :show]
 end
