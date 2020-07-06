37c5ee4b5b2c583cece345f74f7b9c7106c849d4
JaronArmiger <jaron.armiger@gmail.com>
2020-07-06 12:23:11 -0500

    other text_field working

--- app/javascript/scripts/accounts_form.js
+++ app/javascript/scripts/accounts_form.js
@@ -1,4 +1,11 @@
 
 $(document).ready( function() {
-    
+  const otherDiv = $("#other-div");
+  otherDiv.hide();
+  $("#account_institution_type").change( function() {
+  	const selected = $(this).val();
+  	if (selected === "Other") {
+ 	  otherDiv.show();
+  	}
+  });
 });
