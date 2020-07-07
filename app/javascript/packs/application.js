856c010c4a4c8b417baed34cd07f3be95451d9b0
JaronArmiger <jaron.armiger@gmail.com>
2020-07-07 10:12:33 -0500

    remove require directive for webpack-icons-installer from application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("webpack-icons-installer/bootstrap")
+//require("webpack-icons-installer/bootstrap")
 
 import './bootstrap_custom.js'
 import '../stylesheets/application'
