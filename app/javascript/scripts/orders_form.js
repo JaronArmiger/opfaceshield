977ff94f7e15190c9717e9e21e2eaa592769cace
JaronArmiger <jaron.armiger@gmail.com>
2020-07-07 16:59:19 -0500

    write js that unchecks address radio buttons if add new address button is clicked

--- app/javascript/scripts/orders_form.js
+++ app/javascript/scripts/orders_form.js
@@ -8,17 +8,21 @@ $(document).on('turbolinks:load', function() {
 
   const newAddressButton = $('#new-address-btn');
   const newAddressDiv = $('#new-address-div');
+  const radioButtons = $('input[type=radio]');
+
   if (newAddressButton) {
   	newAddressDiv.hide();
     newAddressDiv.toggleClass('showing');
   }
+
   newAddressButton.click(() => {
   	newAddressDiv.toggle();
     if (newAddressDiv.hasClass('showing')) {
       newAddressButton.html('<i class="material-icons">add_circle_outline</i> Add new address</span>');
     } else {
       newAddressButton.html('<i class="material-icons">remove_circle_outline</i> Hide address fields</span>');
+      radioButtons.prop('checked', false);
     }
     newAddressDiv.toggleClass('showing');
-  }) 
+  })
 });
