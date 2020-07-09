6f6e17e9f881cf3f120f3c44ca12dc9ea8d0105e
JaronArmiger <jaron.armiger@gmail.com>
2020-07-09 11:51:56 -0500

    write before actions in application controller that redirect non-admins from admin pages

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,7 @@
+module Admin
+  class OrdersController < ApplicationController
+  	def index
+  	  @orders = Order.all
+  	end
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,22 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	before_action :authenticate_user!
 	before_action :store_user_location!, if: :storable_location?
-
+	#before_action :admin?, if: :admin_controller?
+=begin
+	before_action do |controller|
+	  space = controller.class.name.split("::").first
+	  if space != "Devise" && space == "Admin"
+	  	puts "Admin"
+	  elsif space == "Devise" && space != "Admin"
+	  	puts "Devise"
+	  elsif space != "Devise" && space != "Admin"
+	  	puts "normal"
+	  end
+	  #p space
+	  p current_user
+	end
+=end
+	before_action :admin?, if: :admin_controller?
 	private
 
 	  def storable_location?
@@ -21,5 +36,21 @@ class ApplicationController < ActionController::Base
 	  def after_confirmation_path_for(resource_name, resource)
 	    sign_in(resource)
 	    root_path
+	  end
+
+	  def admin?
+	  	unless current_user && current_user.admin?
+	  	  flash[:notice] = "You must be an admin to access that page ;)"
+	  	  redirect_to root_path
+	  	end
+	  	#puts "are you an admin?"
+	  end
+
+	  def admin_controller?
+	  	prefix = controller_path.split("/").first
+	  	if prefix == "admin"
+	  	  return true
+	  	end
+	  	false
 	  end
 end
