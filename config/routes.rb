787c39fb040788eb5479e92a67c471d43311767b
JaronArmiger <jaron.armiger@gmail.com>
2020-07-16 10:06:15 -0500

    rearrange links for signed in regular user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
   resources :accounts, except: [:index, :destroy]
   resources :addresses, except: [:index, :show]
   resources :orders, except: [:edit, :update]
